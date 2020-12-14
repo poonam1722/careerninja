5e50d2d398ea049f53deaafcca040796dcaa02eb
Poonam Kumari <poonam0502@gmail.com>
2020-12-14 13:08:39 +0530

    Edited app.js

--- server.js
+++ server.js
@@ -25,9 +25,6 @@ connection.once('open', () => {
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
 }
 
 app.get('/location', async (req, res) => {
